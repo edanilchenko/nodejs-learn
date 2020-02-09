193aa8e25fde0daa0ec65681d4533b41401b2e71
Evgeniy <e_danilchenko@outlook.com>
2020-02-09 21:05:52 +0200

    Add REST api to app

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(stylus.middleware(__dirname + '/public'));
 
+app.use(express.favicon());
+app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+
 var bands = ['Cure', 'Clash', 'Damned'];
 app.get('/bands/:name?', function(req, res, next){
     var name = req.params.name;
@@ -19,17 +25,50 @@ app.get('/bands/:name?', function(req, res, next){
     }
     next();
 });
-
 app.get('/bands/', function(req, res){
     res.render('band_list', {title:'Bands', bandsList: bands});
 });
-
 app.get('/bands/:name?', function(req, res){
     res.send('Unknown band!');
 });
-
 app.get('/', function(req, res){
     res.render('index', {title:'Bands', textBody: 'Hello, MyExpress!!!'});
+});
+
+app.get('/add/', function(req, res){
+    res.render('add_form');
+});
+app.post('/add/', function(req, res){
+    bands.push(req.body.band_name);
+    res.render('band_list', {title: 'Bands', bandsList: bands});
+});
+
+app.get('/edit/:name?', function(req, res){
+    res.render('edit_form', {name: req.params.name});
+});
+app.put('/edit/:name?', function(req, res){
+    var name = req.body.name;
+    console.log(req.params);
+    for(key in bands){
+        if(bands[key] === name){
+            console.log('aaaaaaa');
+            bands[key] = req.body.newName;
+        }
+    }
+    res.render('band_list', {title: 'Bands', bandsList: bands});
+});
+
+app.get('/del/:name?', function(req, res){
+    res.render('delete_form', {name: req.params.name})
+});
+app.delete('/del/:name?', function(req, res){
+    var name= req.params.name;
+    for(key in bands){
+        if(bands[key] === name){
+            delete bands[key];
+        }
+    }
+    res.render('band_list', {title:'Bands', bandsList: bands});
 });
 
 app.listen(port);
